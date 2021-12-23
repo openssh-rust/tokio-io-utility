a8e65e0d20082992b144d69cb6bdf3ec35d2ebe9
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-23 18:06:08 +1100

    Simplify `async_write_utility`: Replace trait with regular async func
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/async_write_utility.rs
+++ src/async_write_utility.rs
@@ -1,90 +1,43 @@
-use core::future::Future;
-use core::marker::Sized;
-use core::pin::Pin;
 use core::slice;
-use core::task::{Context, Poll};
 
 use std::io::{IoSlice, Result};
 use tokio::io::AsyncWrite;
+use tokio::io::AsyncWriteExt;
 
-pub struct WriteVectorizedAll<'a, 'b, 'c, T: AsyncWriteUtility + ?Sized>(
-    Pin<&'a mut T>,
-    Option<&'b mut [IoSlice<'c>]>,
-);
-
-impl<T: AsyncWriteUtility + ?Sized> Future for WriteVectorizedAll<'_, '_, '_, T> {
-    type Output = Result<()>;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let slices = self.1.take().unwrap();
-
-        match AsyncWriteUtility::poll_write_vectored_all(self.0.as_mut(), cx, slices) {
-            Poll::Ready(res) => Poll::Ready(res),
-            Poll::Pending => {
-                self.1 = Some(slices);
-                Poll::Pending
-            }
-        }
+pub async fn write_vectored_all<Writer: AsyncWrite + Unpin>(
+    writer: &mut Writer,
+    mut bufs: &mut [IoSlice<'_>],
+) -> Result<()> {
+    if bufs.is_empty() {
+        return Ok(());
     }
-}
 
-pub trait AsyncWriteUtility: AsyncWrite {
-    fn poll_write_vectored_all(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        mut bufs: &mut [IoSlice<'_>],
-    ) -> Poll<Result<()>> {
-        if bufs.is_empty() {
-            return Poll::Ready(Ok(()));
-        }
+    // Loop Invariant: bufs must not be empty
+    'outer: loop {
+        // bytes must be greater than 0
+        let mut bytes = writer.write_vectored(bufs).await?;
 
-        // Loop Invariant: bufs must not be empty
-        'outer: loop {
-            // bytes must be greater than 0
-            let mut bytes = match self.as_mut().poll_write_vectored(cx, bufs) {
-                Poll::Ready(res) => res?,
-                Poll::Pending => return Poll::Pending,
-            };
+        while bufs[0].len() <= bytes {
+            bytes -= bufs[0].len();
+            bufs = &mut bufs[1..];
 
-            while bufs[0].len() <= bytes {
-                bytes -= bufs[0].len();
-                bufs = &mut bufs[1..];
-
-                if bufs.is_empty() {
-                    return Poll::Ready(Ok(()));
-                }
-
-                if bytes == 0 {
-                    continue 'outer;
-                }
+            if bufs.is_empty() {
+                return Ok(());
             }
 
-            let buf = &bufs[0][bytes..];
-            bufs[0] = IoSlice::new(unsafe { slice::from_raw_parts(buf.as_ptr(), buf.len()) });
+            if bytes == 0 {
+                continue 'outer;
+            }
         }
-    }
 
-    /// Equivalent to:
-    ///
-    /// ```ignore
-    /// async fn write_vectored_all(&mut self, bufs: &mut [IoSlice<'_>]) -> Result<()>;
-    /// ```
-    fn write_vectored_all<'a, 'b, 'c>(
-        &'a mut self,
-        bufs: &'b mut [IoSlice<'c>],
-    ) -> WriteVectorizedAll<'a, 'b, 'c, Self>
-    where
-        Self: Unpin,
-    {
-        WriteVectorizedAll(Pin::new(self), Some(bufs))
+        let buf = &bufs[0][bytes..];
+        bufs[0] = IoSlice::new(unsafe { slice::from_raw_parts(buf.as_ptr(), buf.len()) });
     }
 }
-
-impl<T: AsyncWrite + ?Sized> AsyncWriteUtility for T {}
 
 #[cfg(test)]
 mod tests {
-    use super::AsyncWriteUtility;
+    use super::write_vectored_all;
 
     use std::io::IoSlice;
     use std::slice::from_raw_parts;
@@ -108,11 +61,11 @@ mod tests {
                 let w_task = tokio::spawn(async move {
                     let buffer: Vec<u32> = (0..1024).collect();
 
-                    w.write_vectored_all(&mut [as_ioslice(&buffer), as_ioslice(&buffer)])
+                    write_vectored_all(&mut w, &mut [as_ioslice(&buffer), as_ioslice(&buffer)])
                         .await
                         .unwrap();
 
-                    w.write_vectored_all(&mut [as_ioslice(&buffer), as_ioslice(&buffer)])
+                    write_vectored_all(&mut w, &mut [as_ioslice(&buffer), as_ioslice(&buffer)])
                         .await
                         .unwrap();
                 });
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod async_read_utility;
 mod async_write_utility;
 
 pub use async_read_utility::read_exact_to_vec;
-pub use async_write_utility::{AsyncWriteUtility, WriteVectorizedAll};
+pub use async_write_utility::write_vectored_all;
