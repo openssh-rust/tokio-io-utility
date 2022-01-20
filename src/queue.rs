864e7441f784624e3051ec889f08447bd4a56d18
Jiahao XU <Jiahao_XU@outlook.com>
2022-01-20 16:15:27 +1100

    Fix updating `free` & `tail_pending` in `push`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/queue.rs
+++ src/queue.rs
@@ -60,28 +60,15 @@ impl MpScBytesQueue {
 
         let slice_len = slice.len() as u16;
 
-        // Update free
-        let mut free = self.free.load(Ordering::Relaxed);
+        // Update free and tail_pending
+        let mut tail_pending = self.tail_pending.load(Ordering::Relaxed);
+        let mut new_tail_pending;
         loop {
+            let free = self.free.load(Ordering::Relaxed);
             if free < slice_len {
                 return Err(slice);
             }
 
-            match self.free.compare_exchange_weak(
-                free,
-                free - slice_len,
-                Ordering::Relaxed,
-                Ordering::Relaxed,
-            ) {
-                Ok(_) => break,
-                Err(new_free) => free = new_free,
-            }
-        }
-
-        // Update tail_pending
-        let mut tail_pending = self.tail_pending.load(Ordering::Relaxed);
-        let mut new_tail_pending;
-        loop {
             new_tail_pending = u16::overflowing_add(tail_pending, slice_len).0 % (queue_cap as u16);
 
             match self.tail_pending.compare_exchange_weak(
@@ -90,7 +77,10 @@ impl MpScBytesQueue {
                 Ordering::Relaxed,
                 Ordering::Relaxed,
             ) {
-                Ok(_) => break,
+                Ok(_) => {
+                    self.free.fetch_sub(slice_len, Ordering::Relaxed);
+                    break;
+                }
                 Err(new_value) => tail_pending = new_value,
             }
         }
