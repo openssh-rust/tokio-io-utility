21385cf8de0faad583d82ede41fbe40593a31ab9
Jiahao XU <Jiahao_XU@outlook.com>
2022-01-20 14:42:59 +1100

    Run cargo test using thread sanitizer in run_test.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,13 @@ cargo test --all-features -- --nocapture
 
 export RUSTFLAGS='-Zsanitizer=address'
 cargo +nightly test --all-features async_read_utility::tests::test -- --nocapture
-exec cargo +nightly test --all-features queue -- --nocapture
+cargo +nightly test --all-features queue -- --nocapture
+
+export RUSTFLAGS='-Zsanitizer=thread' 
+
+for _ in $(seq 1 10); do
+    cargo +nightly test \
+        -Z build-std \
+        --target --target $(uname -m)-unknown-linux-gnu \
+        --all-features queue::tests::test_par -- --nocapture
+done
