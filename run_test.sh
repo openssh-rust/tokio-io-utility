ea659c03bbfb74455c5a6f26f6f308e800b6973d
Jiahao XU <Jiahao_XU@outlook.com>
2022-01-20 16:14:54 +1100

    Reenable thread sanitizer & Disable miri
    For thread sanitizer error is not false positive and miri is taking too
    long.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_test.sh
+++ run_test.sh
@@ -6,20 +6,18 @@ export RUSTFLAGS='-Zsanitizer=address'
 cargo +nightly test --all-features async_read_utility::tests::test -- --nocapture
 cargo +nightly test --all-features queue -- --nocapture
 
-# Disable thread sanitizer for it often provides false positive.
-#
-#export RUSTFLAGS='-Zsanitizer=thread' 
-#
-#for _ in $(seq 1 10); do
-#    cargo +nightly test \
-#        -Z build-std \
-#        --target $(uname -m)-unknown-linux-gnu \
-#        --all-features queue::tests::test_par -- --nocapture
-#done
+export RUSTFLAGS='-Zsanitizer=thread' 
 
-unset RUSTFLAGS
-export MIRIFLAGS="-Zmiri-disable-isolation"
-exec cargo +nightly miri test \
-    -Z build-std \
-    --target $(uname -m)-unknown-linux-gnu \
-    --all-features queue -- --nocapture
+for _ in $(seq 1 10); do
+    cargo +nightly test \
+        -Z build-std \
+        --target $(uname -m)-unknown-linux-gnu \
+        --all-features queue::tests::test_par -- --nocapture
+done
+
+#unset RUSTFLAGS
+#export MIRIFLAGS="-Zmiri-disable-isolation"
+#exec cargo +nightly miri test \
+#    -Z build-std \
+#    --target $(uname -m)-unknown-linux-gnu \
+#    --all-features queue -- --nocapture
