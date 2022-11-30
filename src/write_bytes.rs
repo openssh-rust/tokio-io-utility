7af087c745b3804faea79008f70bb00783775b7d
Jiahao XU <Jiahao_XU@outlook.com>
2022-11-30 23:40:49 +1100

    Add unit test for `write_all_bytes`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/write_bytes.rs
+++ src/write_bytes.rs
@@ -91,3 +91,145 @@ async fn write_all_bytes_inner(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        iter,
+        num::NonZeroUsize,
+        task::{Context, Poll},
+    };
+
+    use bytes::BytesMut;
+    use tokio::io::AsyncWrite;
+
+    use super::*;
+    use crate::IoSliceExt;
+
+    /// Limit number of bytes that can be sent for each write.
+    struct WriterRateLimit(usize, Vec<u8>);
+
+    impl AsyncWrite for WriterRateLimit {
+        fn poll_write(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buf: &[u8],
+        ) -> Poll<io::Result<usize>> {
+            let n = buf.len().min(self.0);
+            let buf = &buf[..n];
+
+            Pin::new(&mut self.1).poll_write(cx, buf)
+        }
+        fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+            Pin::new(&mut self.1).poll_flush(cx)
+        }
+        fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+            Pin::new(&mut self.1).poll_shutdown(cx)
+        }
+
+        fn poll_write_vectored(
+            mut self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+            bufs: &[io::IoSlice<'_>],
+        ) -> Poll<io::Result<usize>> {
+            let mut cnt = 0;
+            let n = self.0;
+            bufs.iter()
+                .copied()
+                .filter_map(|io_slice| {
+                    (n > cnt).then(|| {
+                        let n = io_slice.len().min(n - cnt);
+                        cnt += n;
+                        &io_slice.into_inner()[..n]
+                    })
+                })
+                .for_each(|slice| {
+                    self.1.extend(slice);
+                });
+
+            Poll::Ready(Ok(cnt))
+        }
+
+        fn is_write_vectored(&self) -> bool {
+            true
+        }
+    }
+
+    #[test]
+    fn test() {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async {
+                let bytes: BytesMut = (0..255).collect();
+                let bytes = bytes.freeze();
+
+                let iter = iter::once(bytes).cycle().take(20);
+
+                let expected_bytes: Vec<u8> = iter.clone().flatten().collect();
+
+                let mut reusable_io_slices = ReusableIoSlices::new(NonZeroUsize::new(3).unwrap());
+
+                // Emulate a pipe where each time only half of the Bytes can be
+                // written.
+                let writer = WriterRateLimit(255 / 2, Vec::new());
+                tokio::pin!(writer);
+
+                write_all_bytes(
+                    writer.as_mut(),
+                    &mut iter.clone().collect(),
+                    &mut reusable_io_slices,
+                )
+                .await
+                .unwrap();
+
+                assert_eq!(writer.1, expected_bytes);
+
+                // Emulate a pipe where each time exactly one Bytes can be
+                // written.
+                writer.0 = 255;
+                writer.1.clear();
+
+                write_all_bytes(
+                    writer.as_mut(),
+                    &mut iter.clone().collect(),
+                    &mut reusable_io_slices,
+                )
+                .await
+                .unwrap();
+
+                assert_eq!(writer.1, expected_bytes);
+
+                // Emulate a pipe where each time one and a half Bytes can be
+                // written.
+                writer.0 = 255 + 255 / 2;
+                writer.1.clear();
+
+                write_all_bytes(
+                    writer.as_mut(),
+                    &mut iter.clone().collect(),
+                    &mut reusable_io_slices,
+                )
+                .await
+                .unwrap();
+
+                assert_eq!(writer.1, expected_bytes);
+
+                // Emulate a pipe where each time one Bytes and a little bit
+                // of the next Bytes can be written.
+                writer.0 = 255 + 5;
+                writer.1.clear();
+
+                write_all_bytes(
+                    writer.as_mut(),
+                    &mut iter.clone().collect(),
+                    &mut reusable_io_slices,
+                )
+                .await
+                .unwrap();
+
+                assert_eq!(writer.1, expected_bytes);
+            });
+    }
+}
